04c5a895c565bd93b74a51438b07b6d8ec3ec5aa
Narayana Samy Kannan <nk@KGFSL.COM>
2017-12-21 14:56:03 +0530

    Overriden http message using gson to Prettify json output.

--- src/main/java/com/naren/webcrawler/WebcrawlerApplication.java
+++ src/main/java/com/naren/webcrawler/WebcrawlerApplication.java
@@ -3,6 +3,7 @@ package com.naren.webcrawler;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,22 +12,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.naren.webcrawler.core.SharedService;
 import com.naren.webcrawler.core.WebCrawler;
 
+@EnableWebMvc
 @Configuration
 @RestController
-@ComponentScan({ "com.naren.webcrawler", "com.naren.webcrawler.*" })
 @SpringBootApplication
-public class WebcrawlerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WebcrawlerApplication.class, args);
-	}
+@ComponentScan({ "com.naren.webcrawler", "com.naren.webcrawler.*" })
+public class WebcrawlerApplication extends WebMvcConfigurerAdapter {
 
 	@Autowired
 	WebCrawler webCrawler;
@@ -34,10 +38,28 @@ public class WebcrawlerApplication {
 	@Autowired
 	SharedService sharedService;
 
+	static {
+		System.setProperty("http.proxyHost", "10.100.1.124");
+		System.setProperty("http.proxyPort", "3128");
+		System.setProperty("https.proxyHost", "10.100.1.124");
+		System.setProperty("https.proxyPort", "3128");
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		GsonHttpMessageConverter msgConverter = new GsonHttpMessageConverter();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		msgConverter.setGson(gson);
+		converters.add(msgConverter);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcrawlerApplication.class, args);
+	}
+
 	@RequestMapping("/{protocol}/{url}")
 	public Map<String, Set<String>> crawl(@PathVariable String protocol, @PathVariable String url)
 			throws IOException, URISyntaxException {
-
 		URL domainUrl = new URL(protocol + "://" + url);
 		sharedService.result.clear();
 		sharedService.domain = domainUrl;
